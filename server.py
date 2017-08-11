b3a306a0efcb6d5effe0081f1ac00b62e7485fee
Marcel Porta Vallés <marcelpv96@gmail.com>
2017-08-11 11:32:47 +0200

    calling func projects by name in url

--- server.py
+++ server.py
@@ -1,13 +1,20 @@
 from flask import Flask, request
-
+from floyd import floyd
 app = Flask(__name__)
 
 
-@app.route('/project', methods=['GET', 'POST'])
+@app.route('/project', methods=['GET'])
 def project():
-    username = request.args.get('name')
-    lgIP = request.args.get('lgip')
+    proj_name = request.args.get('name')
+    lg_IP = request.args.get('lgip')
+    try:
+        proj_func[proj_name](lg_IP, 'lqgalaxy')
+        return ""
+    except KeyError:
+        return "ERROR in project NAME"
 
 
+proj_func = {'floyd': floyd}
 if __name__ == "__main__":
+
     app.run()
