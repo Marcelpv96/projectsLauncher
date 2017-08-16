b5414ec713227ba8b1919bb5890249e756ffde72
Marcel Porta Vallés <marcelpv96@gmail.com>
2017-08-16 12:06:23 +0200

    getting serverip

--- mainscrip.sh
+++ mainscrip.sh
@@ -1,1 +1,2 @@
-curl http://127.0.0.1:5000/project?name=floyd&lgip=127.0.1.1
+serverip=ifconfig | grep "inet 10." | awk '{print $2}'
+curl http://$serverip:5000/project?name=floybd&lgip=10.160.67.206&serverip=$serverip
