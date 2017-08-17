42da84927ed6b639bc364194a5f4624ef53ee6d4
Marcel Porta Vallés <marcelpv96@gmail.com>
2017-08-17 15:41:53 +0200

    remove kmls

--- end_django_proj.sh
+++ end_django_proj.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 bash /flask/end_project.sh manage.py
+for i in `seq 1 7`;
+       do
+               echo "" | sshpass -p lq ssh lg@$1 cat - > /var/www/html/kmls_$i.txt
+       done
--- end_node_proj.sh
+++ end_node_proj.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+for i in `seq 1 7`;
+       do
+               echo "" | sshpass -p lq ssh lg@$1 cat - > /var/www/html/kmls_$i.txt
+       done
