8d61031a33a3b1e274a40daccfc5b2ddd7a7d6d4
lg <liquidgalaxylab@gmail.com>
2017-08-21 17:40:48 +0200

    solving some bugs

--- flask/demos.py
+++ flask/demos.py
@@ -1,13 +1,14 @@
 
 import subprocess
 
-link = subprocess.Popen(
-    "ifconfig | grep 'inet addr:10' | awk '{print $2}'| cut -d ':' -f 2", shell=True)
+link = subprocess.check_output(
+    "ifconfig | grep 'inet addr:10' | awk '{print $2}'| cut -d ':' -f 2", stderr=subprocess.STDOUT,shell=True)
 
 
 def floybd_demo(tour):
     port = ":8000"
-    subprocess.Popen(link + port + tour, shell=True)
+    print link + port + tour
+    subprocess.Popen("curl "+link + port + tour, shell=True)
 
 
 def memories_demo():
@@ -17,7 +18,7 @@ def memories_demo():
 def WikimediaDataProject_demo(tour):
     port = ":8000"
     print link + tour
-    subprocess.Popen(link + port + "/try_demo", shell=True)
+    subprocess.Popen("curl "+link + port + "/try_demo", shell=True)
     time.sleep(2)
     subprocess.Popen(link + port + tour, shell=True)
 
